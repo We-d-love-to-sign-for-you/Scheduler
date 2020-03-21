761e35d77b35de55dbf2f55ddef1aea0d70f2199
Daniel Brinkmann <d-brinkmann@gmx.net>
2020-03-21 14:07:38 +0100

    Set up project

---
+++ src/main/java/org/wedlovetosignforyou/Scheduler.java
@@ -0,0 +1,9 @@
+package org.wedlovetosignforyou;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+public class Scheduler {
+    public String schedule(String skillName, String minSkillLevel, Context context) {
+        return "";
+    }
+}
